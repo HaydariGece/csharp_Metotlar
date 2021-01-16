a20eeeeee8f8d77c993341d08234cd19816f9255
HaydariGece <75258160+HaydariGece@users.noreply.github.com>
2021-01-16 23:47:04 +0300

    Add project files.

---
+++ cshar_Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cshar_Metotlar
+{
+    class SepetManager
+    {
+        public void ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi "+ urun.Adi);
+
+        }
+
+        public void ekle2(string urunAdi, string aciklama, double fiyat, int stokAdeti)
+        {
+            Console.WriteLine("Sepete eklendi " + urunAdi);
+        }
+    }
+}
